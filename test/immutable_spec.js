a3987c714644b66085bde69eb5da03f7f9e02d65
Peter S <s@pramos.me>
2016-08-17 14:56:08 +0200

    Added lists and maps to our first test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,8 +1,10 @@
 import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
 describe('immutability', () => {
     describe('a number', () => {
-        function increment(currentsState) {
-            return currentsState + 1;
+        function increment(currentState) {
+            return currentState + 1;
         }
         it('is immutable', () => {
             let state = 42;
@@ -10,6 +12,54 @@ describe('immutability', () => {
 
             expect(nextState).to.equal(43);
             expect(state).to.equal(42);
+        });
+    });
+
+    describe('a list', () => {
+        function addMovie(currentState, movie) {
+            return currentState.push(movie);
+        }
+
+        it('is immutable', () => {
+            let state = List.of('Trainspotting', '28 Days Later');
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(List.of(
+                'Trainspotting',
+                '28 Days Later',
+                'Sunshine'
+            ));
+            expect(state).to.equal(List.of(
+                'Trainspotting',
+                '28 Days Later'
+            ));
+        });
+    });
+
+    describe('a tree', () => {
+        function addMovie(currentState, movie) {
+            return currentState.update('movies', movies => movies.push(movie));
+        }
+
+        it('is immutable', () => {
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
+            let nextState = addMovie(state, 'Sunshine');
+
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later',
+                    'Sunshine'
+                )
+            }));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later'
+                )
+            }));
         });
     });
 });
